45ff31cc940e9dd3d4278729f7fdfaade98e8c59
Chad <chadananda@gmail.com>
2017-07-04 07:22:36 -0700

    added rule for 's, updated readme, moved functions into an object, updated docs

--- README.md
+++ README.md
@@ -9,16 +9,28 @@ npm install --save bahai-term-phonemes
 
 ### Functionality
 ``` Javascript
-import pho from 'bahai-term-phonemes'
+import trms from 'bahai-term-phonemes'
 
+isPossibleTerm(str)
+// checks if term is likely a Baha'i transliteration
 
+phonemes(word)
+// translates a word to IPA code in double-brackets. example:
+trms.parse("‘Abdu’l-Ḥamíd")
+// output: "[[@ b d @U ? l ? h @ m i: d]]"
 
-pho(str) 
-// identifies Baha'i terms in string and returns modified string replacing any such terms with double-bracketed IPN codes
-// for example: pho.replace("Bahá’u’lláh, Whose Mission")
-// output: [[b @ h A: ? @U ? l l A: h]], Whose Mission
+parse(str) 
+// identifies Baha'i terms in string and returns modified string replacing any such terms with double-bracketed IPA codes, for example: 
+trms.parse("Bahá’u’lláh, Whose Mission...")
+// output: "[[b @ h A: ? @U ? l l A: h]], Whose Mission..."
+
+strip_accents(str)
+// returns ASCII version of string, removing dot-unders, accents and underscores
 ```
  
 ### Demo
-
 Browser jsfiddle demo: https://jsfiddle.net/chadananda/xa7dtvc3/
+
+
+
+
--- bahai-term-phonemes.js
+++ bahai-term-phonemes.js
@@ -1,116 +1,128 @@
-// code to translate one or more Baha'i terms to TTS phonemes
-//
+// Translate Baha'i terms to IPA phonemes for use in TTS
 
 
-module.exports = function (text) {
-  if (!text) return
-  var words = text.split(' ');
-  words.forEach(function(element, index, array){
-    if (_is_term(element)) array[index] = _term_to_ipa(element)
-  });
-  return words.join(' ');
-}
+var term_phonemes = {
 
-function _is_term(term) {
-  term = term.toLowerCase().trim();
-  // trim leading and trailing punctuation
-  var newterm = term.replace(/^[^a-zḥṭẓḍṣ_áíú]/g, '').replace(/[^a-zḥṭẓḍṣ_áíú]$/g, '');
-  // trim contraction suffix and possessives
-  newterm = newterm.replace(/[’']s$/im, '').replace(/n[’']t$/im, '');
-  var isTerm = (newterm != newterm.replace(/[ẓḥṭẓḍṣ_áíú’‘]/g, ''));
-  return isTerm;
-}
+  isPossibleTerm: function (term) {
+    term = term.toLowerCase().trim() 
+    // trim leading and trailing punctuation
+    var newterm = term.replace(/^[^a-zḥṭẓḍṣ_áíú]/g, '').replace(/[^a-zḥṭẓḍṣ_áíú]$/g, '') 
+    // trim contraction suffix and possessives
+    newterm = newterm.replace(/[‘’']s$/im, '').replace(/n[‘’']t$/im, '') 
+    //console.log(newterm)
+    var isTerm = (newterm != newterm.replace(/[ẓḥṭẓḍṣ_áíú’‘']/g, '')) 
+    //console.log(newterm)
+    return isTerm 
+  },
 
-function _strip_accents(term) {
+  parse: function (text) {
+    if (!text) return
+    var trm = this
+    var words = text.split(' ') 
+    words.forEach(function(element, index, array){
+      if (trm.isPossibleTerm(element)) array[index] = trm.phonemes(element, true)
+    }) 
+    return words.join(' ') 
+  }, 
+
+  strip_accents: function (term) {
     var in_chrs =  'àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ',
         out_chrs = 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY',
-        transl = {};
-    eval('var chars_rgx = /['+in_chrs+']/g');
-    for(var i = 0; i < in_chrs.length; i++){ transl[in_chrs.charAt(i)] = out_chrs.charAt(i); }
-    return term.replace(chars_rgx, function(match){ return transl[match]; });
+        transl = {} 
+    eval('var chars_rgx = /['+in_chrs+']/g') 
+    for(var i = 0;  i < in_chrs.length;  i++){ transl[in_chrs.charAt(i)] = out_chrs.charAt(i)  }
+    return term.replace(chars_rgx, function(match){ return transl[match]  }) 
+  },
+
+  phonemes:  function (term, includePunctuation=false) {
+    var original = term;
+    var prefix = term.replace(/^([^a-zḥṭẓḍṣ_áíú]*).*/i, '$1')
+    var suffix = term.replace(/.*?([^a-zḥṭẓḍṣ_áíú]*)$/i, '$1')
+    var term = term.replace(/^[^a-zḥṭẓḍṣ_áíú]/ig, '').replace(/[^a-zḥṭẓḍṣ_áíú]$/ig, '')
+
+    // Bahá’u’lláh ->  ba hah ow lah
+    term = term.toLowerCase().trim();
+    // conver html to glyph
+    term = term.replace(/<u>/g, '_').replace(/<\/u>/g,'');
+    // remove any remaining tags and whitespace
+    term = term.replace(/<(.|\n)*?>/g, '').replace(/\s+/g, ' ').trim();
+    // replace any letters that sound like another
+    term = term.replace(/ḍ/g, 'z').replace(/_dh/g, 'z').replace(/_th/g, 's').replace(/u/g, 'o').replace(/aw/g, 'o');
+    term = term.replace(/_gh/g, 'g');
+    // replace apostrophe ’s with just s 
+    term = term.replace(/([\’\‘\'\`]s)$/m, 's') 
+
+    // connectors
+    term = term.replace(/-i-/g, 'i-')
+    .replace(/’(d-D|_kh-_kh|_sh-_sh|_ch-_ch|_zh-_zh|b-b|p-p|j-j|t-t|d-d|r-r|z-z|s-s|f-f|q-q|k-k|l-l|m-m|n-n|h-h)/, '$1') ;
+    // remove beginning or ending ayn and hamza
+    term = term.replace(/^[’‘']/, '').replace(/[’‘']$/, '');
+
+    var vowels = {
+      'ay' : 'eI',
+      'iy' : 'eI',
+      'ih' : 'eI',
+      'a'  : '@',
+      'á'  : 'A:',
+      'i'  : 'e',
+      'í'  : 'i:',
+      'o'  : '@U',
+      'ú'  : 'u:'
+    };
+    var consonants = {
+      '_kh' : 'x',
+      '_zh' : 'Z',
+      '_sh' : 'S',
+      '_ch' : 'tS',
+      'b'   : 'b',
+      'p'   : 'p',
+      'j'   : 'dZ',
+      't'   : 't',
+      'ṭ'   : 't',
+      'd'   : 'd',
+      'r'   : 'r',
+      'z'   : 'z',
+      'ẓ'   : 'z',
+      's'   : 's',
+      'ṣ'   : 's',
+      'f'   : 'f',
+      'q'   : 'g',
+      'k'   : 'k',
+      'l'   : 'l',
+      'm'   : 'm',
+      'n'   : 'n',
+      'h'   : 'h',
+      'ḥ'   : 'h',
+      'w'   : 'w',
+      'v'   : 'v',
+      'y'   : 'j',
+      '’'   : '?',
+      '‘'   : '?',
+      '-'   : '?',
+    };
+    for(var key in vowels) if (key.length>1) {
+      var regex = new RegExp(key, 'gi');
+      term = term.replace(regex, vowels[key]+' ');
+    }
+    for(var key in consonants) if (key.length>1) {
+      var regex = new RegExp(key, 'gi');
+      term = term.replace(regex, consonants[key]+' ');
+    }
+
+    for(var key in vowels) if (key.length<2) {
+      var regex = new RegExp(key, 'gi');
+      term = term.replace(regex, vowels[key]+' ');
+    }
+    for(var key in consonants) if (key.length<2) {
+      var regex = new RegExp(key, 'gi');
+      term = term.replace(regex, consonants[key]+' ');
+    }
+
+    if (includePunctuation) return prefix +'[['+ term.trim() + ']]'+ suffix
+     else return '[['+ term.trim() + ']]'
+  } 
+
 }
 
-function _term_to_ipa(term) {
-  var original = term;
-  var prefix = term.replace(/^([^a-zḥṭẓḍṣ_áíú]*).*/i, '$1');
-  var suffix = term.replace(/.*?([^a-zḥṭẓḍṣ_áíú]*)$/i, '$1');
-  var term = term.replace(/^[^a-zḥṭẓḍṣ_áíú]/ig, '').replace(/[^a-zḥṭẓḍṣ_áíú]$/ig, '');
-
-   // Bahá’u’lláh ->  ba hah ow lah
-  term = term.toLowerCase().trim();
-  // conver html to glyph
-  term = term.replace(/<u>/g, '_').replace(/<\/u>/g,'');
-  // remove any remaining tags and whitespace
-  term = term.replace(/<(.|\n)*?>/g, '').replace(/\s+/g, ' ').trim();
-  // replace any letters that sound like another
-  term = term.replace(/ḍ/g, 'z').replace(/_dh/g, 'z').replace(/_th/g, 's').replace(/u/g, 'o').replace(/aw/g, 'o');
-  term = term.replace(/_gh/g, 'g');
-
-  // connectors
-  term = term.replace(/-i-/g, 'i-')
-   .replace(/’(d-D|_kh-_kh|_sh-_sh|_ch-_ch|_zh-_zh|b-b|p-p|j-j|t-t|d-d|r-r|z-z|s-s|f-f|q-q|k-k|l-l|m-m|n-n|h-h)/, '$1') ;
-  // remove beginning or ending ayn and hamza
-  term = term.replace(/^[’‘]/, '').replace(/[’‘]$/, '');
-
-  var vowels = {
-    'ay' : 'eI',
-    'iy' : 'eI',
-    'ih' : 'eI',
-    'a'  : '@',
-    'á'  : 'A:',
-    'i'  : 'e',
-    'í'  : 'i:',
-    'o'  : '@U',
-    'ú'  : 'u:'
-  };
-  var consonants = {
-    '_kh' : 'x',
-    '_zh' : 'Z',
-    '_sh' : 'S',
-    '_ch' : 'tS',
-    'b'   : 'b',
-    'p'   : 'p',
-    'j'   : 'dZ',
-    't'   : 't',
-    'ṭ'   : 't',
-    'd'   : 'd',
-    'r'   : 'r',
-    'z'   : 'z',
-    'ẓ'   : 'z',
-    's'   : 's',
-    'ṣ'   : 's',
-    'f'   : 'f',
-    'q'   : 'g',
-    'k'   : 'k',
-    'l'   : 'l',
-    'm'   : 'm',
-    'n'   : 'n',
-    'h'   : 'h',
-    'ḥ'   : 'h',
-    'w'   : 'w',
-    'v'   : 'v',
-    'y'   : 'j',
-    '’'   : '?',
-    '‘'   : '?',
-    '-'   : '?',
-  };
-  for(var key in vowels) if (key.length>1) {
-    var regex = new RegExp(key, 'gi');
-    term = term.replace(regex, vowels[key]+' ');
-  }
-  for(var key in consonants) if (key.length>1) {
-    var regex = new RegExp(key, 'gi');
-    term = term.replace(regex, consonants[key]+' ');
-  }
-
-  for(var key in vowels) if (key.length<2) {
-    var regex = new RegExp(key, 'gi');
-    term = term.replace(regex, vowels[key]+' ');
-  }
-  for(var key in consonants) if (key.length<2) {
-    var regex = new RegExp(key, 'gi');
-    term = term.replace(regex, consonants[key]+' ');
-  }
-
-  return prefix +'[['+ term.trim() + ']]'+ suffix;
-}
+  
+module.exports = term_phonemes
